e0516098bb68183216959d298595d67dac466969
msherga <63604994+msherga@users.noreply.github.com>
2026-07-13 01:01:47 -0500

    Set UTF-8 locale for Shiny runtime

---
+++ BB_Study/R/00-locale.R
@@ -0,0 +1,8 @@
+# Ensure text functions such as gsub() can process UTF-8 values on shinyapps.io.
+locale_result <- try(Sys.setlocale("LC_CTYPE", "C.UTF-8"), silent = TRUE)
+
+if (inherits(locale_result, "try-error") || identical(locale_result, "")) {
+  try(Sys.setlocale("LC_CTYPE", "en_US.UTF-8"), silent = TRUE)
+}
+
+options(encoding = "UTF-8")
